d204b27c84fece3ac50a9365d82d85c15af3b120
jpmeraz <juanpimq@gmail.com>
2022-09-22 22:52:26 -0500

    Código backup

---
+++ backup.ino
@@ -0,0 +1,21 @@
+int luz_out = A3;
+int vent_out = A2;
+int s0und_out = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(luz_out, OUTPUT);
+  pinMode(vent_out, OUTPUT);
+  pinMode(s0und_out, OUTPUT);
+  
+  digitalWrite(luz_out, HIGH);
+  digitalWrite(vent_out, HIGH);
+
+}
+
+void loop() {
+
+  
+  // put your main code here, to run repeatedly:
+
+}
