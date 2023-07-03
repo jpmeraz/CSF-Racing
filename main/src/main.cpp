ea5de9450ad6d2803569ee77df759fd810affe48
jpmeraz <84814896+jpmeraz@users.noreply.github.com>
2023-07-03 00:46:14 -0600

    Implementación del GPS, sensado de swches y relays

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -10,23 +10,62 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 
-
 //Termómetro
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 
+//GPS
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <Arduino.h>
 
-//U8G2LIB_ST7920_128X64 u8g2(10, 11, 13, U8G_PIN_NONE);
-U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 36, /* data=*/ 34, /* CS=*/ 35, /* reset=*/ U8X8_PIN_NONE); // Feather HUZZAH ESP8266, E=clock=14, RW=data=13, RS=CS
+
+/*Protitipos de funciones*/
+void bootUp_screen();
+void clear_screen();
+void mostrar_display(float temperaturaObjeto, float temperaturaAmbiente);
+void error_handling(int componente, int debug);
+bool newData();
+int Velocidad();
+bool Switches(int Switch, int Relay);
+
+
+/*Variables*/
+#define lcdE 36
+#define lcdRW 34
+#define lcdRS 35
+#define gpsTX 18
+#define gpsRX 19
+#define Switch1 31
+#define Switch2 32
+#define Switch3 33
+#define Relay1 37
+#define Relay2 38
+#define Relay3 39
+
+float temperaturaAmbiente = 0;
+float temperaturaObjeto = 0;
+int velocidad = 0;
+bool isSwitch1 = false;
+bool isSwitch2 = false;
+bool isSwitch3 = false;
+
+
+/*Objetos*/
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ lcdE, /* data=*/ lcdRW, /* CS=*/ lcdRS, /* reset=*/ U8X8_PIN_NONE); //E=clock=14, RW=data=13, RS=CS
 Adafruit_MLX90614 termometroIR = Adafruit_MLX90614();
+SoftwareSerial mygps(gpsRX, gpsTX);
+TinyGPSPlus gps;
 
 
 /*Array para almacenar los códigos de estatus:
 [0] --> Termómetro
-[1] --> Pantalla LCD*/
-int estatus [2]; //Espacio para 2 códigos de estatus.
+[1] --> Pantalla LCD
+[2] --> GPS
+*/
+int estatus [3]; //Espacio para 2 códigos de estatus.
+String componentes [3] = {"Termómetro", "Pantalla LCD", "GPS"}; //Array para almacenar los nombres de los componentes.
 int intento = 0; //Contador interno de void setup()
-
 
 // Logotipo de la escudería
 #define BORREGOS_SF_RACING_TEAM_W 43
@@ -65,72 +104,87 @@ static const unsigned char BORREGOS_SF_RACING_TEAM[] U8X8_PROGMEM = {
    0xfe, 0xff, 0xff, 0x7f, 0xfc, 0x03, 0xfc, 0xff, 0xff, 0x7f, 0xfe, 0x01,
    0xf8, 0xff, 0xff, 0x7f, 0xfe, 0x00, 0xe0, 0xff, 0xff, 0x7f, 0x3e, 0x00 };
 
-  const char DEGREE_SYMBOL[] = { 0xB0, '\0' };
 
 
 void setup()
 {
-  Serial.begin(9600);
-  /*BOOTUP TERMOMETRO IR*/
-  //Sí el código de estatus del termómetro es 200, se ejecuta el código correspondiente.
-  intento = 0;
-  /*while (intento < 3) {
-    if (termometroIR.begin() == 1){
-      estatus[0] = 200;
-      error_handling("Termometro", 200); //Imprime código de estatus
-      intento = 4;
-    }
-    else{
-      estatus[0] = 401;
-      error_handling("Termometro", 401); //Imprime código de estatus
-    }
-    intento++;
-    delay(5000);
-  }
-  */
-
-  intento = 0; // Se reinicia el contador de intentos.
-
-  /*BOOTUP LCD*/
-  /* Se intenta iniciar el LCD 3 veces, si no se obtiene respuesta de u8g.begin(), se matiene el código de estatus 401
+  Serial.begin(115200);
+  /*BOOTUP LCD
+  Se intenta iniciar el LCD 3 veces, si no se obtiene respuesta de u8g.begin(), se da el código de estatus 401
   y se continúa con el programa.*/
   while (intento < 3){
     if (u8g2.begin() != 0){
       u8g2.setColorIndex(1); // pixels on
       u8g2.enableUTF8Print();
-      estatus[1] = 200;
-      error_handling("LCD", 200); //Imprime código de estatus
+      error_handling(1, 200); // Se almacena e imprime el código de estatus.
       bootUp_screen();
       intento = 4;
-      delay(5000);
-      clear_screen();
-      Serial.println("Intento " + intento);
     }
     else{
       estatus[1] = 501;
-      //error_handling("LCD", 501); //Imprime código de estatus
     }
     intento++;
   }
-delay(500);
+  intento = 0; // Se reinicia el contador de intentos.
+
+  /*BOOTUP TERMOMETRO IR
+  Se intenta iniciar el termómetro 3 veces, si no se obtiene respuesta de termometroIR.begin(), se da el 
+  código de estatus 401*/
+  while (intento < 3) {
+    if (termometroIR.begin() == 1){
+      error_handling(0, 200); // Se almacena e imprime el código de estatus.
+      intento = 4;
+    }
+    else{
+      estatus[0] = 401;
+      error_handling(0, 401); // Se almacena e imprime el código de estatus.
+    }
+    intento++;
+    delay(5000);
+  }
+  intento = 0; // Se reinicia el contador de intentos.
+
+  /*BOOTUP GPS*/
+  mygps.begin(9600);
+
+  /*BOOTUP SWITCHES*/
+  pinMode(Switch1, INPUT);
+  pinMode(Switch2, INPUT);
+  pinMode(Switch3, INPUT);
+  pinMode(Relay1, OUTPUT);
+  pinMode(Relay2, OUTPUT);
+  pinMode(Relay3, OUTPUT);
+
+  delay(5000);
+  clear_screen();
 }
 
 
 void loop()
 {
-  Serial.println("Si serví");
-  float temperaturaAmbiente = 30;//termometroIR.readAmbientTempC();
-  float temperaturaObjeto = 30; //termometroIR.readObjectTempC();
   /*LECTURA DE TEMPERATURA*/
-  /*if (estatus[0] == 200){
+  if (estatus[0] == 200){
+    temperaturaAmbiente = termometroIR.readAmbientTempC();
+    temperaturaObjeto = termometroIR.readObjectTempC();
     if (!(isnan(temperaturaAmbiente))){
       delay(5);
     }
     else{
       estatus[0] = 402; // El modificar el estatus, loop no volverá a ejecutar el código del termometro.
-      error_handling("Termometro", 402); //Imprime código de estatus
+      error_handling(0, 402); //Imprime código de estatus
     }
-  }*/
+  }
+
+  /*LECTURA DE VELOCIDAD*/
+  if (newData())
+  {
+    velocidad = Velocidad();
+  }
+
+  /*SWITCHES*/
+  isSwitch1 = Switches(Switch1, Relay1);
+  isSwitch2 = Switches(Switch2, Relay2);
+  isSwitch3 = Switches(Switch3, Relay3);
 
   /*IMPRIMIR EN LCD*/
   if (estatus[1] == 200){
@@ -139,11 +193,10 @@ void loop()
       mostrar_display(temperaturaObjeto, temperaturaAmbiente);
     } while(u8g2.nextPage());
   }
-  delay(100);
+
 }
 
 /*FUNCIONES*/
-
 void bootUp_screen()
 {
   // graphic commands to redraw the complete screen should be placed here
@@ -155,11 +208,10 @@ void bootUp_screen()
   {
     u8g2.drawXBMP(0, 0, BORREGOS_SF_RACING_TEAM_W, BORREGOS_SF_RACING_TEAM_H, BORREGOS_SF_RACING_TEAM); // (x, y, width*8, height)
     u8g2.setFont(u8g2_font_helvB10_tf);
-    u8g2.drawStr(46, 31, "JARVIS");
+    u8g2.drawStr(46, 31, "MARTIN");
     u8g2.setFont(u8g2_font_6x13_tf);
     u8g2.drawStr(46, 45, "By SF RACING");
   } while (u8g2.nextPage());
-
   delay(6000);
 } 
 
@@ -187,18 +239,56 @@ void mostrar_display(float temperaturaObjeto, float temperaturaAmbiente)
     u8g2.drawStr(20, 14, "#10 - SERGIO Z.");
 
     u8g2.drawStr(8, 60, "AMBNT");
-    u8g2.drawUTF8(38, 60, DEGREE_SYMBOL);
+    u8g2.drawUTF8(38, 60, "°");
     u8g2.drawStr(46, 60, "C"); //16 Px de ancho -- 8 pixeles mas que el simbolo °
 
     //u8g2.drawStr(10, 60, "KM/H"); //32 Px de ancho
     u8g2.drawStr(72, 60, "MOTOR");
-    u8g2.drawUTF8(102, 60, DEGREE_SYMBOL);
+    u8g2.drawUTF8(102, 60, "°");
     u8g2.drawStr(110, 60, "C"); //16 Px de ancho -- 8 pixeles mas que el simbolo °
 
 }
 
 // Función para imprimir los códigos de estatus.
-void error_handling(String componente, int debug){
-  Serial.print(componente + ": ");
-  Serial.println(debug);
+void error_handling(int componente, int debug){
+  estatus[componente] = debug; //Se guarda el código de estatus en el array.
+  Serial.print(componentes[componente] + ": "); //Se imprime el nombre del componente.
+  Serial.println(debug); //Se imprime el código de estatus.
+}
+
+bool newData(){
+  for (unsigned long start = millis(); millis() - start < 200;)
+  {
+    while (mygps.available())
+    {
+      if (gps.encode(mygps.read()))
+        return true;
+    }
+  }
+  return false;
+}
+
+int Velocidad()
+{
+  if (gps.location.isValid() == 1)
+  {
+    return gps.speed.kmph();
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+bool Switches(int Switch, int Relay){
+    if (digitalRead(Switch) == HIGH)
+  {
+    digitalWrite(Relay, HIGH);
+    return true;
+  }
+  else
+  {
+    digitalWrite(Relay, LOW);
+    return false;
+  }
 }
